1dbe5a55e693ed35c6af8ac4f8b42e7d498d80c2
MaryRyabova <mary_95_30@mail.ru>
2017-01-09 22:05:10 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,58 +1,106 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-/*Однажды известный профессор обнаружил описания k конечных автоматов. 
-По его мнению, нетривиальность конечного автомата, имеющего n состояний и m переходов, 
-можно описать целым числом d = 19m + (n + 239)*(n + 366) / 2 . 
-Чем больше d, тем больший интерес для науки представляет изучение его свойств.
-Помогите профессору вычислить нетривиальность имеющихся у него автоматов и определить самый интересный из них. */
+//Даны координаты вершин некоторого треугольника и координаты некоторой точки внутри него.
+//Найти расстояние от данной точки до ближайшей стороны треугольника.
+
+
+//Алгоритм взят из источника http://grafika.me/node/237
+int ifLineCrossed( float ax, float ay, float bx, float by, float cx, float cy, float dx, float dy )
+{
+    int ifYes = 0;
+    float abx, aby, acx, acy, adx, ady, cdx, cdy, cbx, cby, cax, cay, v1, v2, v3, v4;
+
+    abx = bx - ax ;
+    aby = by - ay;
+
+    acx = cx - ax;
+    acy = cy - ay;
+
+    adx = dx - ax;
+    ady = dy - ay;
+
+    cdx = dx - cx;
+    cdy = dy - cy;
+
+    cbx = bx - cx;
+    cby = by - cy;
+
+    cax = ax - cx;
+    cay = ay - cy;
+
+    v1 = ( abx * acy ) - ( aby * acx);
+    v2 = ( abx * ady ) - ( aby * adx );
+    v3 = ( cdx * cay ) - ( cdy - cax );
+    v4 = ( cdx - cby ) - ( cdy - cbx );
+
+    if ( (v1 * v2 < 0) && (v3 * v4 < 0) )
+    {
+        ifYes++;
+    }
+
+    return ifYes;
+}
+
+//Расстояние между точкой и вершиной треугольника
+double distCount( double ax, double ay, double bx, double by)
+{
+    return sqrt( ( ( bx - ax ) * ( bx - ax ) ) + ( ( by - ay ) * ( by - ay ) ) );
+
+}
 
 void main()
 {
-	int n, m, counter = 0;
-	char answer;
-	double d;
-	double *resMas = new double[counter];
+    double x1, x2, x3, px, y1, y2, y3, py, d1, d2, d3;
 
-	do
-	{
-		cout << "How much states do you have?";
-		cin >> n;
+    double ox, oy = 0;
+    int crossLineIndicate = 0;
 
-		cout << "How much transitions do you have?";
-		cin >> m;
+    cout << "Enter coordinates of your triangle. Type at first X coord. ";
+    cout << "First top A is:\n "; 
+    cin >> x1 >> y1;
+    cout << "Second top B is:\n "; 
+    cin >> x2 >> y2;
+    cout << "And third C:\n "; 
+    cin >> x3 >> y3;
+    cout << "Your point to chack it's distance:\n "; 
+    cin >> px >> py;
 
-		d = double(19 * m + (n + 239) * (n + 366) / 2);
-		resMas[counter] = d;
-		counter++;
+    ox = px;
 
 
-		cout << "Do you want rapid enetering the data? (y/n)";
-		cin >> answer;
 
-	} while (answer != 'n');
+    crossLineIndicate += ifLineCrossed( ox, oy, px, py, x1, y1, x2, y2);
+    crossLineIndicate += ifLineCrossed( ox, oy, px, py, x3, y3, x2, y2);
+    crossLineIndicate += ifLineCrossed( ox, oy, px, py, x1, y1, x3, y3);
 
-	cout << endl;
+    if ( crossLineIndicate % 2 == 0 )
+    {
+        cout << "\nYour point lie in triangle! " 
+            "\nNow we will detect the smallest distance betwen point and triangle side!";
+    }
+    else
+    {
+        cout << "Please, enter another point that will be lie in the triangle!";
+        cin >> px >> py;
+    }
 
-	for (int i = 0; i < counter; i++)
-	{
-		cout << resMas[i] << endl;
-	}
+    d1 = distCount( px, py, x1, y1 );
+    d2 = distCount( px, py, x2, y2 );
+    d3 = distCount( px, py, x3, y3 );
 
-	cout << endl;
+    if ( d1 > d2 && d1 > d3 )
+        cout << "Your point close to side BC.";
 
-	double maxVal;
-	for (int i = 0; i < counter; i++)
-	{
-		for (int j = i + 1; j < counter; j++)
-		{
-			if (resMas[i] < resMas[j])
-				maxVal = resMas[j];
-			else
-				maxVal = resMas[i];
-		}
-	}
+    else  if ( d2 > d1 && d2 > d3 )
+        cout << "Your point close to side AC.";
 
-	cout << maxVal << endl;
-	system("pause");
+    else  if ( d3 > d1 && d3 > d2 )
+        cout << "Your point close to side AB.";
+
+
+
+    system( "pause" );
 }
